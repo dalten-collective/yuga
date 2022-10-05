9a7c3e631903a12053e90f242657cc455c760e3b
ya1sec <ankit@ya1sec.com>
2022-10-05 13:26:40 -0400

    indigo login

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,22 @@
-import { Box, Row, StatelessTextInput, Text, Center } from "@tlon/indigo-react";
+import { Box, Row, StatelessTextInput, Text, Center, Button } from "@tlon/indigo-react";
 
 function Header () {
   return (
-	<Center>
+	<Box p="0">
+		  <Row
+			  justifyContent="center"
+			  alignItems="center"
+		  //   borderBottom="1px solid rgba(0, 0, 0, 0.1)"
+		  >
+			<Box>
+				<p className="logotext">
+					Cyclopaedia
+				</p>
+				<p>
+					Log in, drop out.
+				</p>
+			</Box>
+		</Row>
 	  <Row
 		justifyContent="center"
 		alignItems="center"
@@ -15,13 +29,14 @@ function Header () {
 				  </Text>
 			  </Box>
 		  </Box> */}
-		  <Box p="12px">
+		  <Box p="0">
 			  <StatelessTextInput
 				  className="input"
 				  color={"white"}
-				  placeholder="Ship Name"
+				  placeholder="Ship name"
 				  backgroundColor="rgba(0, 0, 0, 0.04)"
-				  borderRadius="4px"
+				  borderColor={"#c3bdbda5"}
+				  borderRadius="8px"
 				  fontWeight={400}
 				  height={40}
 				  width={256}
@@ -32,7 +47,8 @@ function Header () {
 				  color={"white"}
 				  placeholder="Ship URL"
 				  backgroundColor="rgba(0, 0, 0, 0.04)"
-				  borderRadius="4px"
+				  borderColor={"#c3bdbda5"}
+				  borderRadius="8px"
 				  fontWeight={400}
 				  height={40}
 				  width={256}
@@ -44,14 +60,17 @@ function Header () {
 				  color={"white"}
 				  placeholder="+code"
 				  backgroundColor="rgba(0, 0, 0, 0.04)"
-				  borderRadius="4px"
+				  borderColor={"#c3bdbda5"}
+				  borderRadius="8px"
 				  fontWeight={400}
 				  height={40}
 				  width={256}
 			  />
+			<br />
+			<button className="create-button">Connect Urbit</button>
 		  </Box>
 	  </Row>
-	</Center>
+	</Box>
   );
 }
 
