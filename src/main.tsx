5329def05366a79af98aecd5f60e75425e3baa23
ya1sec <ankit@ya1sec.com>
2022-10-06 10:46:46 -0400

    indigo component example page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Example from './Example'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Example />
   </React.StrictMode>
 )
