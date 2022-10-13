addbd0c584351b49e86ce08f5e2988abbe6d7d80
ya1sec <ankit@ya1sec.com>
2022-10-13 19:21:25 -0400

    dependency resolution

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as ReactDOM from 'react-dom'
+// import createRoot
 import App from './App'
-import Example from './Example'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
-    {/* <Example /> */}
     <App />
   </React.StrictMode>
-)
+, document.getElementById('root'))
