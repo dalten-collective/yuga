c9a758fe80c9292b681d51779a62dad9638402ef
ya1sec <ankit@ya1sec.com>
2022-10-13 21:17:33 -0400

    bigint build fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // Fix Big integer literals are not supported when targeting lower than ES2020
+  build: {
+    target: 'esnext',
+  },
+
 })
