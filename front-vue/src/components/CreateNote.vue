5ce805cfbb6ff24639b050382fcdae0f44be35b7
vinney cavallo <vcavallo@gmail.com>
2023-01-02 14:53:32 -0500

    Add post preview to post composer

--- front-vue/src/components/CreateNote.vue
+++ front-vue/src/components/CreateNote.vue
@@ -12,29 +12,55 @@
       </div>
     </div>
 
-    <div>
+    <div class="mb-2">
       <label for="title-field">
         Post title
         <input placeholder="" id="title-field" type="text" v-model="title" />
       </label>
     </div>
 
-    <div>
-      <label for="body-field">
-        Post body
-        <textarea rows="10" class="w-full" v-model="content" />
-      </label>
+    <div class="flex flex-row mb-2">
+      <div class="mr-2">
+        <label for="url-field">
+          Content URL
+          <input placeholder="Fetch markdown from URL..." id="url-field" type="text" v-model="articleURL" />
+        </label>
+      </div>
+      <div>
+        <button :disabled="articleURL == ''" @click="getMarkdown" :class="articleURL === '' ? 'loading-btn' : 'action-btn' ">{{ articleURL === '' ? 'Enter URL' : 'Fetch!' }}</button>
+      </div>
     </div>
 
-    <button @click="sendPost" class="action-btn">Post</button>
+    <div class="flex flex-row">
+      <div class="flex-grow w-1/2 h-[40em]">
+        <label for="body-field">
+          Post body
+          <textarea :disabled="loading" class="w-full h-[37em]" v-model="content" />
+        </label>
+      </div>
+      <div class="w-1/2 ml-2 h-[38em]">
+        Post preview:
+        <div class="border border-2 rounded-sm shadow-inner">
+          <div v-html="postPreview" class="h-[38em] p-2 overflow-scroll rendered-markdown">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button :disabled="loading" :class="loading ? ['loading-btn'] : ['action-btn']" @click="sendPost">{{ loading ? 'Fetching...' : 'Post' }}</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import * as T from "@/types"
 import * as ramaAPI from '@/api/ramaAPI'
 import { sigShip } from "@/helpers"
+
+import TurndownService from 'turndown'
+import { extract } from '@extractus/article-extractor'
+import { marked } from 'marked'
+import * as DOMPurify from 'dompurify'
 
 interface Props {
   host: T.Ship;
@@ -42,9 +68,36 @@ interface Props {
 }
 const props = defineProps<Props>();
 
+const articleURL = ref('');
 const title = ref('');
 const content = ref('');
 const cover = ref('');
+const loading = ref(false);
+
+const getMarkdown = () => {
+  loading.value = true
+  const t = new TurndownService();
+
+  const encodedUrl = encodeURIComponent(articleURL.value);
+  const reqUrl = "https://extract-article.deta.dev/?url=" + encodedUrl;
+
+  fetch(reqUrl, {method: 'GET', redirect: 'follow'})
+  .then(response => response.json())
+  .then(result => {
+  // extract(articleURL.value)
+      console.log('article ', result.data)
+      const markdownContent = t.turndown(result.data.content)
+      content.value = markdownContent
+      loading.value = false
+    }).catch((err) => {
+      loading.value = false
+      content.value = "Could not fetch markdown"
+    })
+}
+
+const postPreview = computed(() => {
+  return marked(content.value)
+})
 
 const sendPost = () => {
   const who = sigShip(props.host)
